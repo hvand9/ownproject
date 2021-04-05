b32fa37254d7f343e519ce4da6b3522d827f76e3
hvand9 <54908812+hvand9@users.noreply.github.com>
2021-04-05 23:15:34 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/product">Product</router-link> 
+
+    
+  </div>
+  <router-view/>
+</template>
+
+<style>
+  #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+  background-color: antiquewhite;
+}
+
+#nav {
+  padding: 30px;
+  font-size: 100px;
+ 
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+* {
+    box-sizing: border-box;
+  }
+  
+  .column {
+    float: left;
+    width: 33.33%;
+    padding: 5px;
+  }
+  
+  /* Clearfix (clear floats) */
+  .row::after {
+    content: "";
+    clear: both;
+    display: table;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome to the clothing shop</h1>
+    <p>Here will you be able to find many different kinds of clothing brands</p>
+    <p>Maybe we have something you will be looking for?</p>
+    <h2>We take all kinds of payment</h2>
+    <img alt="Vue logo" src="../assets/payment.png">
+    
+    <h2>Subscribe to our newsletter</h2>
+
+<form action="/action_page.php">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="John"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
